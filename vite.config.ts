5c813fff4c752d18e3c6963a663f33f3d6567b8d
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-04-22 00:42:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/portfolio/' : '/',
-  plugins: [react()],
-  server: {
-    port: 3000,   // 👨‍💻 Local dev server port
-    open: true,   // 🔓 Opens browser automatically
-  },
-  build: {
-    outDir: 'dist',    // 📦 Output folder for production build
-    sourcemap: true,   // 🧭 Helpful for debugging
-  },
-}));
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+
+  return {
+    base: isBuild ? '/resportfolio/' : '/',
+    plugins: [react()],
+    server: {
+      port: 3000,   // 👨‍💻 Local dev server port
+      open: true,   // 🔓 Opens browser automatically
+    },
+    build: {
+      outDir: 'dist',    // 📦 Output folder for production build
+      sourcemap: true,   // 🧭 Helpful for debugging
+    },
+  };
+});
